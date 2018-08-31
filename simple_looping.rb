220847e30209af6c84c9195d14583cfcebeeccef
Steven Zapata <szapata427@gmail.com>
2018-08-31 16:32:19 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -20,9 +20,9 @@ end
 end
 
 
-
+# code your solution here using the "times" keyword
 def times_iterator(number_of_times)
-  # code your solution here using the "times" keyword
+  
   phrase = "Welcome to Flatiron School's Web Development Course!"
   
 end
